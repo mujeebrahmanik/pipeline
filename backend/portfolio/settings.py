d0f002b621ea223169fe171c89f77bb76780e51f
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2025-08-27 22:27:33 +0530

    Added docker CD

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -114,7 +114,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add this at the bottom of settings.py
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
